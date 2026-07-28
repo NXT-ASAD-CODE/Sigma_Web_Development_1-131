bf605f7becf1f4995cc0cff0bccfc3f6a4bb6eb7
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-28 19:25:39 +0500

    Video 75

--- 075 VIDEO/promise.js
+++ 075 VIDEO/promise.js
@@ -1,12 +1,18 @@
 console.log("Promise");
  
 let prom1 = new Promise((reslove,reject)=>{
-setTimeout(()=>{
-    console.log("promise");
-    reslove("Resolve")
-},3000);
+    let a = Math.random();
+    if(a<0.5){
+        reject("No Random number")
+    }
+    else{
+
+        setTimeout(()=>{
+            console.log("promise");
+            reslove("Resolve")
+        },3000);
+    }
 })
 prom1.then((a)=>{
     console.log(a);
-    
 })
