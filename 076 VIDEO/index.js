bb512217bbc3f3f32c7b1524715efa3b27fdf761
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-31 19:42:50 +0500

    Video 76

---
+++ 076 VIDEO/index.js
@@ -0,0 +1,10 @@
+function getData(){
+
+}
+console.log("Loading Modules");
+console.log("another code");
+console.log("data Loading");
+let data = getData()
+console.log("process data");
+console.log();
+console.log();
